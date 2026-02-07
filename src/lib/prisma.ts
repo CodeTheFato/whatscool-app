47de0bd37ba46b44e7762db5376ecd6075e4bfb4
jovemdan <danilogdev@gmail.com>
2026-02-07 17:40:39 -0300

    fix: ajuste prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,7 +4,14 @@ import { PrismaClient } from '@prisma/client'
 
 const connectionString = `${process.env.DATABASE_URL}`
 
-const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
 
-export { prisma }
+const adapter = new PrismaPg({ connectionString })
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
+}
